08819bfb2b832e6eaa8aeddf255629d69c23e922
zabdielfer <zabdielfer@hotmail.com>
2020-09-11 18:26:17 -0500

    Initial commit and root route

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HelloApp::Application.config.secret_key_base = '5fc0201b6b7549e1c046b291279409cc9ea1264460635033301a62ebe06935e9a55c1b07f5a193e73aba1eab5ecd74ff72d41e2190c7aa775865253393ee33c7'
